59595fd529b44a315e889c0c40d24bd4052a42bc
kzd76 <szternak@gmail.com>
2025-04-11 23:14:23 +0200

    Add files via upload

--- software/msbasic/iscntc.s
+++ software/msbasic/iscntc.s
@@ -1,32 +1,18 @@
 .segment "CODE"
+
 ; ----------------------------------------------------------------------------
 ; SEE IF CONTROL-C TYPED
 ; ----------------------------------------------------------------------------
-.ifndef CONFIG_CBM_ALL
-.include "cbm_iscntc.s"
-.endif
-.ifdef KBD
-.include "kbd_iscntc.s"
-.endif
-.ifdef OSI
-.include "osi_iscntc.s"
-.endif
-.ifdef APPLE
-.include "apple_iscntc.s"
-.endif
-.ifdef KIM
-.include "kim_iscntc.s"
-.endif
-.ifdef MICROTAN
-.include "microtan_iscntc.s"
-.endif
-.ifdef AIM65
-.include "aim65_iscntc.s"
-.endif
-.ifdef SYM1
-.include "sym1_iscntc.s"
-.endif
-.ifdef EATER
-.include "eater_iscntc.s"
-.endif
-;!!! runs into "STOP"
+
+ISCNTC:
+        jsr CHRIN
+        bcc not_cntc                        ; C is set by CHRIN if any key is pressed
+        cmp #3
+        bne not_cntc
+        jmp is_cntc
+
+not_cntc:
+        rts
+
+is_cntc:
+        ; Fall through
